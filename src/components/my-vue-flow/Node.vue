a8ce16f953df3cc491bcb5c603972db9ab518d08
s1riller <miheev.saveliy.2002@gmail.com>
2023-10-18 00:23:00 +0800

    first commit

---
+++ src/components/my-vue-flow/Node.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="node">
+    <!-- Содержимое узла -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Node',
+  import { VueFlow, useVueFlow } from '@vue-flow/core'
+
+const initialNodes = ref([
+  {
+    id: '1',
+    position: { x: 50, y: 50 },
+    label: 'Node 1',
+  }
+])
+const { addNodes } = useVueFlow({
+  nodes: initialNodes,
+})
+
+onMounted(() => {
+  // Add an element after mount
+  addNodes([
+    {
+      id: '2',
+      position: { x: 150, y: 50 },
+      label: 'Node 2',
+    }
+  ])
+})
+}
+
+</script>
+
+<style>
+@import '@vue-flow/core/dist/style.css';
+@import '@vue-flow/core/dist/theme-default.css';
+</style>
